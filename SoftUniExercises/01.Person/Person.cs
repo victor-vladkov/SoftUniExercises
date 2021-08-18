23c2fc2d892c16f0ae236b7cc4125be2df95ff3f
Dilyana Toteva <dtoteva@david.bg>
2021-08-18 15:22:30 +0300

    Add "Person" project.

---
+++ SoftUniExercises/01.Person/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace _01.Person
+{
+	public class Person
+	{
+		private string name;
+		private int age;
+
+		public Person(string name, int age)
+		{
+			this.Name = name;
+			this.Age = age;
+		}
+
+		public string Name
+		{
+			get { return this.name; }
+			set { this.name = value; }
+		}
+
+		public int Age
+		{
+			get { return this.age; }
+			set { this.age = value; }
+		}
+
+		public override string ToString()
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+			return stringBuilder.ToString();
+		}
+	}
+
+	public class Child : Person
+	{
+		public Child(string name, int age) : base(name, age)
+		{
+		}
+	}
+}
---
+++ SoftUniExercises/01.Person/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.Person
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+			Child child = new Child(name, age);
+			Console.WriteLine(child);
+		}
+	}
+}
